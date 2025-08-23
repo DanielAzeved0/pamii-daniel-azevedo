d630b855f2de2492ac5c637af5502dfd51d97fe0
EduardoRomaniniPessoa <eduardo.pessoa@etec.sp.gov.br>
2025-08-22 22:21:37 -0300

    O Inicio de tudo

---
+++ Aula03/app/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+	
+export default function index() {
+  return (
+    <Box
+  className="bg-primary-500 p-5"
+>
+  <Text className='text-typography-0'>
+    This is the Box
+  </Text>
+</Box>
+  );
+}
